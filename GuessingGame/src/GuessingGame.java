abaf537bb0a41712444e5f6eb6567dfe688caef2
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2017-09-25 12:56:32 -0400

    First code

---
+++ GuessingGame/src/GuessingGame.java
@@ -0,0 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class GuessingGame 
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String s = input.next();
+		System.out.println("Nice to meet you" + " " + s);
+		//System.out.println("How old are you bud?");
+		boolean isNumber = false;
+		while(!isNumber)
+		{	
+			try
+			{
+				System.out.println("How old are you bud?");
+				input.nextInt();
+				isNumber = true;
+			}
+			catch(InputMismatchException e)
+			{
+				System.out.println("That is not a number");
+			}
+		}
+		//System.out.println("Cool, I'm also" + " " + x + " years old." );
+	}
+
+}
